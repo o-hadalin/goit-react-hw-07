76fb6d680ccc8753ff0a11c249d84f0153a62de6
O. HADALIN <dyblikat777@gmail.com>
2025-01-23 23:52:53 +0200

    Dell persistReducer(), persistStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['items'],
-};
-
 const rootReducer = combineReducers({
-  contacts: persistReducer(contactsPersistConfig, contactsReducer),
+  contacts: contactsReducer,
   filters: filtersReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-      },
-    }),
+  middleware: getDefaultMiddleware => getDefaultMiddleware(),
 });
-
-export const persistor = persistStore(store);
